54ef1e499dbcccd8371d624238c5c7f1e5d2b19a
denis <denis300605@mail.ru>
2024-04-27 23:06:18 +0700

    improvement: updated exifInterface lib; performance may be increased

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("androidx.exifinterface:exifinterface:1.3.6")
+    implementation(libs.androidx.exifinterface)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
