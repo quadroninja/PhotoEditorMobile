489dacf877dbf6cf1d62c6ebe54cccb610df8880
denis <denis300605@mail.ru>
2024-05-07 23:45:18 +0700

    refactor

--- app/src/main/java/com/hitsproject/photoeditor/Processing.kt
+++ app/src/main/java/com/hitsproject/photoeditor/Processing.kt
@@ -2,12 +2,6 @@ package com.hitsproject.photoeditor
 
 import android.graphics.Bitmap
 import android.graphics.Color
-import kotlin.math.max
-import kotlin.math.min
-import kotlin.math.abs
-import kotlin.math.sqrt
-import kotlin.math.exp
-import kotlin.math.pow
 
 class Processing {
     fun applyBlackAndWhiteFilter(image: Bitmap): Bitmap {
@@ -15,22 +9,18 @@ class Processing {
         val height = image.height
         val pixels = IntArray(width * height)
 
-        // Получаем пиксели изображения в виде массива
         image.getPixels(pixels, 0, width, 0, 0, width, height)
 
-        // Применяем фильтр к пикселям
         for (i in pixels.indices) {
             val pixel = pixels[i]
             val red = Color.red(pixel)
             val green = Color.green(pixel)
             val blue = Color.blue(pixel)
 
-            // Вычисляем среднее значение RGB для получения оттенка серого
             val average = (red + green + blue) / 3
             pixels[i] = Color.rgb(average, average, average)
         }
 
-        // Создаем новое отфильтрованное изображение
         val filteredBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         filteredBitmap.setPixels(pixels, 0, width, 0, 0, width, height)
 
