be9b5ff44b64541b257262f5e7f4d39e8220c9f6
denis <denis300605@mail.ru>
2024-05-16 17:28:51 +0700

    reworked design

--- app/src/main/java/com/hitsproject/photoeditor/MainActivity.kt
+++ app/src/main/java/com/hitsproject/photoeditor/MainActivity.kt
@@ -4,14 +4,18 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 lateinit var image: Bitmap
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var image: Bitmap
+    private lateinit var bottomSheetDialog: BottomSheetDialog
     private lateinit var photoPicker: PhotoPicker
     private lateinit var process: Processing
 
@@ -22,52 +26,18 @@ class MainActivity : AppCompatActivity() {
         photoPicker = PhotoPicker(this)
         process = Processing()
 
-        val button = findViewById<FloatingActionButton>(R.id.AddPhoto)
-        button.setOnClickListener {
+        val addPhotoButton = findViewById<FloatingActionButton>(R.id.AddPhoto)
+        addPhotoButton.setOnClickListener {
             photoPicker.pickPhoto(this)
         }
 
         val navigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navigation.menu.findItem(R.id.show_bottom_sheet).isVisible = true
+        navigation.menu.findItem(R.id.show_bottom_sheet).isChecked = true
         navigation.setOnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.bw -> {
-                    image = process.applyBlackAndWhiteFilter(image)
-                    val imageView = findViewById<ImageView>(R.id.ViewImage)
-                    imageView.setImageBitmap(image)
-                    true
-                }
-                R.id.sepia -> {
-                    image = process.applySepiaFilter(image)
-                    val imageView = findViewById<ImageView>(R.id.ViewImage)
-                    imageView.setImageBitmap(image)
-                    true
-                }
-                //R.id.negative -> {
-                    //image = process.applyNegativeFilter(image)
-                    //val imageView = findViewById<ImageView>(R.id.ViewImage)
-                    //imageView.setImageBitmap(image)
-                    //true
-                //}
-                R.id.navigation_rotate ->
-                {
-                    image = process.rotate(image, 70.0)
-                    val imageView = findViewById<ImageView>(R.id.ViewImage)
-                    imageView.setImageBitmap(image)
-                    true
-                }
-                R.id.navigation_resize ->
-                {
-                    image = process.resize(image, 0.1)
-                    val imageView = findViewById<ImageView>(R.id.ViewImage)
-                    imageView.setImageBitmap(image)
-                    true
-
-                }
-                R.id.unsharp_mask ->
-                {
-                    image = process.unsharpMask(image)
-                    val imageView = findViewById<ImageView>(R.id.ViewImage)
-                    imageView.setImageBitmap(image)
+            when (item.itemId) {
+                R.id.show_bottom_sheet -> {
+                    showBottomSheetMenu()
                     true
                 }
                 else -> false
@@ -75,14 +45,59 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun showBottomSheetMenu() {
+        bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_menu)
+
+        bottomSheetDialog.findViewById<TextView>(R.id.bw_menu_item)?.setOnClickListener {
+            image = process.applyBlackAndWhiteFilter(image)
+            updateImageView()
+            bottomSheetDialog.dismiss()
+        }
+
+        bottomSheetDialog.findViewById<TextView>(R.id.sepia_menu_item)?.setOnClickListener {
+            image = process.applySepiaFilter(image)
+            updateImageView()
+            bottomSheetDialog.dismiss()
+        }
+
+        bottomSheetDialog.findViewById<TextView>(R.id.rotate_menu_item)?.setOnClickListener {
+            image = process.rotate(image, 70.0)
+            updateImageView()
+            bottomSheetDialog.dismiss()
+        }
+
+        bottomSheetDialog.findViewById<TextView>(R.id.resize_menu_item)?.setOnClickListener {
+            image = process.resize(image, 0.1)
+            updateImageView()
+            bottomSheetDialog.dismiss()
+        }
+
+        bottomSheetDialog.findViewById<TextView>(R.id.unsharp_mask_menu_item)?.setOnClickListener {
+            image = process.unsharpMask(image)
+            updateImageView()
+            bottomSheetDialog.dismiss()
+        }
+
+        bottomSheetDialog.findViewById<TextView>(R.id.negative_filter_item)?.setOnClickListener {
+            image = process.applyNegativeFilter(image)
+            updateImageView()
+            bottomSheetDialog.dismiss()
+        }
+
+        bottomSheetDialog.show()
+    }
+
+    private fun updateImageView() {
+        val imageView = findViewById<ImageView>(R.id.ViewImage)
+        imageView.setImageBitmap(image)
+    }
+
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         val bitmap = photoPicker.onActivityResult(requestCode, resultCode, data)
-        if (bitmap != null) {
-            val imageView = findViewById<ImageView>(R.id.ViewImage)
-            image = bitmap
-            imageView.setImageBitmap(bitmap)
-        }
+        image = bitmap
+        updateImageView()
     }
 }
